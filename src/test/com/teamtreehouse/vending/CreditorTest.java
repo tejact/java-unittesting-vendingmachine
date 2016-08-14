983ff9b51df6ae6d8f6d4a4a58879b54fb926734
Teja <Teja@Teja-PC>
2016-08-14 10:57:50 -0400

    Added dummy test and eclipse folder structure

---
+++ src/test/com/teamtreehouse/vending/CreditorTest.java
@@ -0,0 +1,29 @@
+package com.teamtreehouse.vending;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CreditorTest {
+
+	@Test
+	public void testAddFunds() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeduct() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRefund() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAvailableFunds() {
+		fail("Not yet implemented");
+	}
+
+}

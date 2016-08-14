51b71219a2b320702f157272adefe652a662e3e7
Teja <Teja@Teja-PC>
2016-08-14 12:10:50 -0400

    Testcases added in creditorTest

--- src/test/com/teamtreehouse/vending/CreditorTest.java
+++ src/test/com/teamtreehouse/vending/CreditorTest.java
@@ -1,29 +1,52 @@
 package com.teamtreehouse.vending;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CreditorTest {
 
+	Creditor creditor;
+	
+	@Before
+	public void setUp() {
+	 creditor = new Creditor();
+
+	}
+
+	
 	@Test
-	public void testAddFunds() {
-		fail("Not yet implemented");
+	public void addingFundsIncrementsAvailableFunds() {
+		//Arrange
+		
+		//Act
+		creditor.addFunds(50);
+		creditor.addFunds(50);
+		
+		//Assert
+		assertEquals(100, creditor.getAvailableFunds());
+	}
+
+
+
+	@Test
+	public void refundingReturnsAllAvailableFunds() {
+		
+		creditor.addFunds(100);
+		int refund = creditor.refund();
+		
+		assertEquals(100, refund);
+		assertEquals(0,creditor.getAvailableFunds());
 	}
 
 	@Test
-	public void testDeduct() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testRefund() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetAvailableFunds() {
-		fail("Not yet implemented");
+	public void refundingMakesAvailableFundsZero() {
+		
+		creditor.addFunds(100);
+		creditor.refund();
+		
+		assertEquals(0,creditor.getAvailableFunds());
 	}
 
 }
